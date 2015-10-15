0a044f6cf7892138cce5c5d0493055588c0fe918
Olmo Mezger <olmo@green-and-energy.com>
2015-10-15 14:07:43 -0600

    help window.

--- www/js/views/help.js
+++ www/js/views/help.js
@@ -20,7 +20,21 @@ var HelpView = Backbone.View.extend({
 
     this.unselectMenu();
     $("#help").addClass("is-active"); // add the active class
+
+    var that = this;
+    setTimeout(function(){
+        that.setHeight();
+    },200)
+
+
     return this;
+  },
+  setHeight:function(){
+    // this function sets the height of the text box above the email button.
+
+    var mainContainerHeight = $('.main-container').height();
+    var marginBottom = 130; // in px. 
+    $('.fixedHeightHelpBox').css({'height':mainContainerHeight - marginBottom + "px"});
   }
 
 });
