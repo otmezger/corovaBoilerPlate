2c842c5b70ad7769123bb858fc743697b8516922
Olmo Mezger <olmo@green-and-energy.com>
2015-10-15 10:38:28 -0600

    working on the design of the detailsView. added basic functionality to the upload picture button.

--- www/js/views/newReportDetails.js
+++ www/js/views/newReportDetails.js
@@ -1,14 +1,12 @@
 var NewReportDetailsView = Backbone.View.extend({
   viewTitle: 'Detalles del reporte',
-  /*
-  tagName: "",
-  className: "",
+
   events: {
-    "click .icon":          "open",
+    "click .newPhoto":          "uploadPicture",
     "click .button.edit":   "openEditDialog",
     "click .button.delete": "destroy"
   },
-  */
+
   initialize: function() {
     //this.listenTo(this.model, "change", this.render);
   },
@@ -19,8 +17,23 @@ var NewReportDetailsView = Backbone.View.extend({
     //$("#whatever").addClass("active"); // add the active class
 
     this.unselectMenu();
+    var that = this;
+    setTimeout(function(){
+      that.adjustHeights();
+    },200);
+    this.delegateEvents();
     $("#newReportDetails").addClass("is-active"); // add the active class
     return this;
+  },
+  adjustHeights:function(){
+    var width = $('.photoContainer').width();
+    $('.photoContainer').css({'height':width+'px'});
+    $('.photoContainer>p').css({'line-height':width+'px'});
+  },
+  uploadPicture:function(){
+    $('.photosRow').prepend('<div class="topcoat-grid__column--3"><div class="photoContainer photoTemp"></div></div> <!-- end of column -->');
+    this.adjustHeights();
+
   }
 
 });
